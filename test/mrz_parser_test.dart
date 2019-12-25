2f9b6567f6af66fdc975a44d25f10df0389af730
Anna Domashych <anna.domashych@gmail.com>
2019-12-25 22:48:19 +0200

    Implement TD2 and TD3 tests

--- test/mrz_parser_test.dart
+++ test/mrz_parser_test.dart
@@ -56,21 +56,6 @@ void main() {
             ]));
   });
 
-  test('correct mrz input parses', () {
-    // Arrange
-    const mrzLines = <String>[
-      'P<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<<<<<<<<<',
-      'L898902C36UTO7408122F1204159ZE184226B<<<<<10'
-    ];
-    const parsed = MRZResult();
-
-    // Act
-    final result = MRZParser.parse(mrzLines);
-
-    // Assert
-    expect(result, parsed);
-  });
-
   test('any TD1 format input parses', () {
     // Arrange
     const mrzLines = <String>[
@@ -87,13 +72,25 @@ void main() {
     expect(result, parsed);
   });
 
-  test('any TD2 format input parses', () {
+  test('TD2 format input parses', () {
     // Arrange
     const mrzLines = <String>[
-      '012345678901234567890123456789012345',
-      '012345678901234567890123456789012345',
+      'P<D<<MUSTERMANN<<ERIKA<<<<<<<<<<<<<<',
+      'C01X00T478D<<6408125F2702283<<<<<<<4'
     ];
-    const parsed = MRZResult();
+    final parsed = MRZResult(
+      documentType: 'P',
+      countryCode: 'D',
+      surnames: 'MUSTERMANN',
+      givenNames: 'ERIKA',
+      documentNumber: 'C01X00T47',
+      nationalityCountryCode: 'D',
+      birthDate: DateTime(1964, 08, 12),
+      sex: Sex.female,
+      expiryDate: DateTime(2027, 02, 28),
+      personalNumber: '',
+      personalNumber2: null,
+    );
 
     // Act
     final result = MRZParser.parse(mrzLines);
@@ -102,13 +99,25 @@ void main() {
     expect(result, parsed);
   });
 
-  test('any TD3 format input parses', () {
+  test('TD3 format input parses', () {
     // Arrange
     const mrzLines = <String>[
-      '01234567890123456789012345678901234567890123',
-      '01234567890123456789012345678901234567890123',
+      'P<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<<<<<<<<<',
+      'L898902C36UTO7408122F1204159ZE184226B<<<<<10'
     ];
-    const parsed = MRZResult();
+    final parsed = MRZResult(
+      documentType: 'P',
+      countryCode: 'UTO',
+      surnames: 'ERIKSSON',
+      givenNames: 'ANNA MARIA',
+      documentNumber: 'L898902C3',
+      nationalityCountryCode: 'UTO',
+      birthDate: DateTime(1974, 08, 12),
+      sex: Sex.female,
+      expiryDate: DateTime(2012, 04, 15),
+      personalNumber: 'ZE184226B<<<<<10',
+      personalNumber2: null,
+    );
 
     // Act
     final result = MRZParser.parse(mrzLines);
