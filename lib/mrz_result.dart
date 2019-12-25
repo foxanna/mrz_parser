1a74dd430668967829919edc85438a595b2d4f87
Anna Domashych <anna.domashych@gmail.com>
2019-12-25 22:48:35 +0200

    Refactor MRZResult

--- lib/mrz_result.dart
+++ lib/mrz_result.dart
@@ -1,3 +1,5 @@
+enum Sex { none, male, female }
+
 class MRZResult {
   const MRZResult({
     this.documentType,
@@ -6,16 +8,11 @@ class MRZResult {
     this.givenNames,
     this.documentNumber,
     this.nationalityCountryCode,
-    this.birthdate,
+    this.birthDate,
     this.sex,
     this.expiryDate,
     this.personalNumber,
     this.personalNumber2,
-    this.isDocumentNumberValid,
-    this.isBirthdateValid,
-    this.isExpiryDateValid,
-    this.isPersonalNumberValid,
-    this.allCheckDigitsValid,
   });
 
   final String documentType;
@@ -24,15 +21,40 @@ class MRZResult {
   final String givenNames;
   final String documentNumber;
   final String nationalityCountryCode;
-  final DateTime birthdate; // `null` if formatting failed
-  final String sex; // `null` if formatting failed
-  final DateTime expiryDate; // `null` if formatting failed
+  final DateTime birthDate;
+  final Sex sex;
+  final DateTime expiryDate;
   final String personalNumber;
-  final String personalNumber2; // `null` if not provided
+  final String personalNumber2;
 
-  final bool isDocumentNumberValid;
-  final bool isBirthdateValid;
-  final bool isExpiryDateValid;
-  final bool isPersonalNumberValid;
-  final bool allCheckDigitsValid;
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MRZResult &&
+          runtimeType == other.runtimeType &&
+          documentType == other.documentType &&
+          countryCode == other.countryCode &&
+          surnames == other.surnames &&
+          givenNames == other.givenNames &&
+          documentNumber == other.documentNumber &&
+          nationalityCountryCode == other.nationalityCountryCode &&
+          birthDate == other.birthDate &&
+          sex == other.sex &&
+          expiryDate == other.expiryDate &&
+          personalNumber == other.personalNumber &&
+          personalNumber2 == other.personalNumber2;
+
+  @override
+  int get hashCode =>
+      documentType.hashCode ^
+      countryCode.hashCode ^
+      surnames.hashCode ^
+      givenNames.hashCode ^
+      documentNumber.hashCode ^
+      nationalityCountryCode.hashCode ^
+      birthDate.hashCode ^
+      sex.hashCode ^
+      expiryDate.hashCode ^
+      personalNumber.hashCode ^
+      personalNumber2.hashCode;
 }
