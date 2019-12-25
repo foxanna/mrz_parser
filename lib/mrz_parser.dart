b4b8a3d25c71172aca2a07f4e8477f5f41ff0ebb
olexale <olexa.le@gmail.com>
2019-12-25 12:26:48 +0200

    initial commit

---
+++ lib/mrz_parser.dart
@@ -0,0 +1,7 @@
+library mrz_parser;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ lib/mrz_result.dart
@@ -0,0 +1,38 @@
+class MRZResult {
+  const MRZResult(
+    this.documentType,
+    this.countryCode,
+    this.surnames,
+    this.givenNames,
+    this.documentNumber,
+    this.nationalityCountryCode,
+    this.birthdate,
+    this.sex,
+    this.expiryDate,
+    this.personalNumber,
+    this.personalNumber2,
+    this.isDocumentNumberValid,
+    this.isBirthdateValid,
+    this.isExpiryDateValid,
+    this.isPersonalNumberValid,
+    this.allCheckDigitsValid,
+  );
+
+  final String documentType;
+  final String countryCode;
+  final String surnames;
+  final String givenNames;
+  final String documentNumber;
+  final String nationalityCountryCode;
+  final DateTime birthdate; // `null` if formatting failed
+  final String sex; // `null` if formatting failed
+  final DateTime expiryDate; // `null` if formatting failed
+  final String personalNumber;
+  final String personalNumber2; // `nulll` if not provided
+
+  final bool isDocumentNumberValid;
+  final bool isBirthdateValid;
+  final bool isExpiryDateValid;
+  final bool isPersonalNumberValid;
+  final bool allCheckDigitsValid;
+}
