4959e44979da9f0edb1ddd42f8f8e5a5166d7e4e
Anna Domashych <anna.domashych@gmail.com>
2019-12-25 16:32:17 +0200

    Check for invalid input

--- lib/mrz_parser.dart
+++ lib/mrz_parser.dart
@@ -7,9 +7,35 @@ class MRZParser {
   MRZParser._();
 
   static MRZResult parse(List<String> input) {
-    if (input == null) {
+    if (!_isValidInput(input)) {
       return null;
     }
+
+    switch (_getMRZFormat(input)) {
+      case _MRZFormat.td1:
+        break;
+      case _MRZFormat.td2:
+        break;
+      case _MRZFormat.td3:
+        break;
+      default:
+        return null;
+    }
+
     return const MRZResult();
+  }
+
+  static bool _isValidInput(List<String> input) =>
+      input != null && (input.length == 2 || input.length == 3);
+
+  static _MRZFormat _getMRZFormat(List<String> input) {
+    switch (input.length) {
+      case 2:
+        return _MRZFormat.unknown;
+      case 3:
+        return _MRZFormat.unknown;
+      default:
+        return _MRZFormat.unknown;
+    }
   }
 }
