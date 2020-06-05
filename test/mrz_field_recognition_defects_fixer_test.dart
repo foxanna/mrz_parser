30447ed02999b20f8208217e190965cc04bf3bf3
Anna Domashych <anna.domashych@gmail.com>
2020-06-05 15:38:58 +0300

    Split FieldParser and FieldRecognitionDefectsFixer tests

---
+++ test/mrz_field_recognition_defects_fixer_test.dart
@@ -0,0 +1,54 @@
+import 'package:mrz_parser/mrz_parser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('fixes document type', () {
+    expect(MRZFieldRecognitionDefectsFixer.fixDocumentType('V'), 'V');
+    expect(MRZFieldRecognitionDefectsFixer.fixDocumentType('P<<'), 'P<<');
+    expect(MRZFieldRecognitionDefectsFixer.fixDocumentType('0128'), 'OIZB');
+    expect(MRZFieldRecognitionDefectsFixer.fixDocumentType('<'), '<');
+  });
+
+  test('fixes check digit', () {
+    expect(MRZFieldRecognitionDefectsFixer.fixCheckDigit('8'), '8');
+    expect(MRZFieldRecognitionDefectsFixer.fixCheckDigit('<6<'), '<6<');
+    expect(MRZFieldRecognitionDefectsFixer.fixCheckDigit('0QUDIZB'), '0000128');
+    expect(MRZFieldRecognitionDefectsFixer.fixCheckDigit('<'), '<');
+  });
+
+  test('fixes date', () {
+    expect(MRZFieldRecognitionDefectsFixer.fixDate('190213'), '190213');
+    expect(MRZFieldRecognitionDefectsFixer.fixDate('19021<'), '19021<');
+    expect(MRZFieldRecognitionDefectsFixer.fixDate('0QUDIZB'), '0000128');
+    expect(MRZFieldRecognitionDefectsFixer.fixDate('<'), '<');
+  });
+
+  test('fixes sex', () {
+    expect(MRZFieldRecognitionDefectsFixer.fixSex('M'), 'M');
+    expect(MRZFieldRecognitionDefectsFixer.fixSex('F'), 'F');
+    expect(MRZFieldRecognitionDefectsFixer.fixSex('P'), 'F');
+    expect(MRZFieldRecognitionDefectsFixer.fixSex('<'), '<');
+  });
+
+  test('fixes country code', () {
+    expect(MRZFieldRecognitionDefectsFixer.fixCountryCode('UA'), 'UA');
+    expect(MRZFieldRecognitionDefectsFixer.fixCountryCode('D<<'), 'D<<');
+    expect(MRZFieldRecognitionDefectsFixer.fixCountryCode('0128'), 'OIZB');
+    expect(MRZFieldRecognitionDefectsFixer.fixCountryCode('<'), '<');
+  });
+
+  test('fixes names', () {
+    expect(
+        MRZFieldRecognitionDefectsFixer.fixNames('<SURNAME<<'), '<SURNAME<<');
+    expect(MRZFieldRecognitionDefectsFixer.fixNames('D<<'), 'D<<');
+    expect(MRZFieldRecognitionDefectsFixer.fixNames('0128'), 'OIZB');
+    expect(MRZFieldRecognitionDefectsFixer.fixNames('<'), '<');
+  });
+
+  test('fixes nationality', () {
+    expect(MRZFieldRecognitionDefectsFixer.fixNationality('UA'), 'UA');
+    expect(MRZFieldRecognitionDefectsFixer.fixNationality('D<<'), 'D<<');
+    expect(MRZFieldRecognitionDefectsFixer.fixNationality('0128'), 'OIZB');
+    expect(MRZFieldRecognitionDefectsFixer.fixNationality('<'), '<');
+  });
+}
--- test/mrz_fields_parser_test.dart
+++ test/mrz_fields_parser_test.dart
@@ -3,91 +3,72 @@ import 'package:mrz_parser/mrz_parser.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('formats document number', () {
+  test('parses document number', () {
     expect(MRZFieldParser.parseDocumentNumber('FHDJEURI3'), 'FHDJEURI3');
-    expect(MRZFieldParser.parseDocumentNumber('FHDJEURI3<'), 'FHDJEURI3');
+    expect(MRZFieldParser.parseDocumentNumber('R3427<<'), 'R3427');
     expect(MRZFieldParser.parseDocumentNumber('<<<<<'), '');
   });
 
-  test('formats document type', () {
+  test('parses document type', () {
     expect(MRZFieldParser.parseDocumentType('V'), 'V');
     expect(MRZFieldParser.parseDocumentType('P<<'), 'P');
-    expect(MRZFieldParser.parseDocumentType('0128'), 'OIZB');
     expect(MRZFieldParser.parseDocumentType('<<<<'), '');
   });
 
-  test('formats country code', () {
+  test('parses country code', () {
     expect(MRZFieldParser.parseCountryCode('UA'), 'UA');
     expect(MRZFieldParser.parseCountryCode('D<<'), 'D');
-    expect(MRZFieldParser.parseCountryCode('0128'), 'OIZB');
     expect(MRZFieldParser.parseCountryCode('<<<<'), '');
   });
 
-  test('formats names', () {
-    const equality = DeepCollectionEquality();
+  test('parses names', () {
+    final equality = const DeepCollectionEquality().equals;
     expect(
-        equality.equals(
-            MRZFieldParser.parseNames('<<SURNAME<<GIVEN<NAMES<<<<<<'),
+        equality(MRZFieldParser.parseNames('<<SURNAME<<GIVEN<NAMES<<<<<<'),
             ['SURNAME', 'GIVEN NAMES']),
-        true);
+        isTrue);
     expect(
-        equality.equals(MRZFieldParser.parseNames('<<SURNAME<<NAME<<<<<<'),
+        equality(MRZFieldParser.parseNames('<<SURNAME<<NAME<<<<<<'),
             ['SURNAME', 'NAME']),
-        true);
+        isTrue);
     expect(
-        equality.equals(
+        equality(
             MRZFieldParser.parseNames('<<SURNAME<<<<<<<<<'), ['SURNAME', '']),
-        true);
-    expect(equality.equals(MRZFieldParser.parseNames('<<<<<<<<<<<'), ['', '']),
-        true);
+        isTrue);
+    expect(
+        equality(MRZFieldParser.parseNames('<<<<<<<<<<<'), ['', '']), isTrue);
   });
-//
-//  test('formats check digit', () {
-//    expect(MRZFieldFormatter.formatCheckDigit('8'), '8');
-//    expect(MRZFieldFormatter.formatCheckDigit('<6<'), '<6<');
-//    expect(MRZFieldFormatter.formatCheckDigit('0QUDIZB'), '0000128');
-//    expect(MRZFieldFormatter.formatCheckDigit('<<<<'), '<<<<');
-//  });
 
-  test('formats nationality', () {
+  test('parses nationality', () {
     expect(MRZFieldParser.parseNationality('UA'), 'UA');
     expect(MRZFieldParser.parseNationality('D<<'), 'D');
-    expect(MRZFieldParser.parseNationality('0128'), 'OIZB');
     expect(MRZFieldParser.parseNationality('<<<<'), '');
   });
-//
-//  test('formats date', () {
-//    expect(MRZFieldFormatter.formatDate('190213'), '190213');
-//    expect(MRZFieldFormatter.formatDate('<190213<<'), '190213');
-//    expect(MRZFieldFormatter.formatDate('0QUDIZB'), '0000128');
-//    expect(MRZFieldFormatter.formatDate('<<<<'), '');
-//  });
 
-  test('formats birth date', () {
+  test('parses birth date', () {
     expect(MRZFieldParser.parseBirthDate('170213'), DateTime(2017, 02, 13));
     expect(MRZFieldParser.parseBirthDate('190213'), DateTime(2019, 02, 13));
-    expect(MRZFieldParser.parseBirthDate('200213'), DateTime(1920, 02, 13));
+    expect(MRZFieldParser.parseBirthDate('200213'), DateTime(2020, 02, 13));
     expect(MRZFieldParser.parseBirthDate('210213'), DateTime(1921, 02, 13));
     expect(MRZFieldParser.parseBirthDate('770213'), DateTime(1977, 02, 13));
   });
 
-  test('formats expiry date', () {
+  test('parses expiry date', () {
     expect(MRZFieldParser.parseExpiryDate('170213'), DateTime(2017, 02, 13));
     expect(MRZFieldParser.parseExpiryDate('710213'), DateTime(1971, 02, 13));
     expect(MRZFieldParser.parseExpiryDate('700213'), DateTime(2070, 02, 13));
     expect(MRZFieldParser.parseExpiryDate('690213'), DateTime(2069, 02, 13));
   });
 
-  test('formats optional data', () {
+  test('parses optional data', () {
     expect(MRZFieldParser.parseOptionalData('FHDJEURI3'), 'FHDJEURI3');
-    expect(MRZFieldParser.parseOptionalData('FHDJEURI3<'), 'FHDJEURI3');
+    expect(MRZFieldParser.parseOptionalData('<GDJSKJ34<'), 'GDJSKJ34');
     expect(MRZFieldParser.parseOptionalData('<<<<<'), '');
   });
 
-  test('formats sex', () {
+  test('parses sex', () {
     expect(MRZFieldParser.parseSex('M'), Sex.male);
     expect(MRZFieldParser.parseSex('F'), Sex.female);
-    expect(MRZFieldParser.parseSex('P'), Sex.female);
     expect(MRZFieldParser.parseSex('<'), Sex.none);
     expect(MRZFieldParser.parseSex('<<<<<'), Sex.none);
   });
